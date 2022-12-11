c8dec402ca437ec767b03c7c9207829548b124b9
garudanish <hyeonsu.kim.dev@gmail.com>
2022-12-11 11:35:49 +0900

    3.3 Testing delete and create

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -47,4 +47,47 @@ describe('MoviesService', () => {
       }
     });
   });
+
+  describe('deleteOne()', () => {
+    it('deletes a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        year: 2015,
+        genres: ['test'],
+      });
+
+      const beforeDelete = service.getAll().length;
+
+      service.deleteOne(1);
+
+      const afterDelete = service.getAll().length;
+
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+
+    it('should return a 404', () => {
+      try {
+        service.deleteOne(999);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toEqual(`Movie with Id 999 not found.`);
+      }
+    });
+  });
+
+  describe('create()', () => {
+    it('should create a movie', () => {
+      const beforeCreate = service.getAll().length;
+
+      service.create({
+        title: 'Test Movie',
+        year: 2015,
+        genres: ['test'],
+      });
+
+      const afterCreate = service.getAll().length;
+
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
 });
