b75d40e1fd459b82c2429e3e5e6d1fe389b8420f
garudanish <hyeonsu.kim.dev@gmail.com>
2022-12-11 12:46:59 +0900

    nestjs/swagger 라이브러리에 맞춰 ApiProperty 데코레이터 선언

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNumber, IsOptional } from 'class-validator';
 
 export class CreateMovieDto {
+  @ApiProperty()
   @IsString()
   readonly title: string;
 
+  @ApiProperty()
   @IsNumber()
   readonly year: number;
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsOptional()
   readonly genres: string[];
