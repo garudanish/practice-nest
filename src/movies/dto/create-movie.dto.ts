4f54557aa6a65904d1e27aa5031a7b7e77fc444c
garudanish <hyeonsu.kim.dev@gmail.com>
2022-12-11 10:55:52 +0900

    2.5 DTOs and Validation part Two

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNumber } from 'class-validator';
+import { IsString, IsNumber, IsOptional } from 'class-validator';
 
 export class CreateMovieDto {
   @IsString()
@@ -8,5 +8,6 @@ export class CreateMovieDto {
   readonly year: number;
 
   @IsString({ each: true })
+  @IsOptional()
   readonly genres: string[];
 }
